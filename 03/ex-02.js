8ab8c46c044aa8ecc4e61930758ba348684e217b
Anh Tung Bui <anhtung.bui@gmail.com>
2020-09-04 19:48:52 +0200

    Add homework for Tutoring No.3

---
+++ 03/ex-02.js
@@ -0,0 +1,23 @@
+/*
+Fullname and Email
+source: edabit
+
+Create the instance properties fullname and email in the Employee class. 
+Given a person's first and last names:
+
+Form the fullname by simply joining the first and last name together, separated by a space.
+Form the email by joining the first and last name together with a . in between, 
+and follow it with @company.com at the end. Make sure everything is in lowercase.
+
+** Examples
+
+emp1 = Employee("John", "Smith")
+emp1.fullname ➞ "John Smith"
+
+emp2 = Employee("Mary",  "Sue")
+emp2.email ➞ "mary.sue@company.com"
+
+emp3 = Employee("Antony", "Walker")
+emp3.firstname ➞ "Antony"
+
+*/
