44d587d139dd1216bdcfb898ebea0010d2f81238
Anh Tung Bui <anhtung.bui@gmail.com>
2021-04-24 12:13:27 +0200

    Add Products component

--- 14/angular-crash-course/src/app/products/products.component.ts
+++ 14/angular-crash-course/src/app/products/products.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  pageTitle = 'Product List';
+  showImage = false;
+  imageWidth = 100;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  products = [
+    {
+      productId: 1,
+      productName: 'Hammer',
+      code: 'h001',
+      available: 'March 24, 2021',
+      price: 8.123456,
+      rating: 5,
+      imageUrl: 'https://via.placeholder.com/350x150',
+    },
+    {
+      productId: 2,
+      productName: 'Axe',
+      code: 'A001',
+      available: 'March 30, 2021',
+      price: 1200.20,
+      rating: 4,
+      imageUrl: '',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleImage() {
+    this.showImage = !this.showImage;
   }
-
 }
